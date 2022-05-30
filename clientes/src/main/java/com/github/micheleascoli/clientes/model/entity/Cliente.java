14b1f31511c3d5ef73654b19cac1f74fc242b564
Michele Ascoli <micheleascoli17@gmail.com>
2022-05-30 14:58:13 -0500

    Desenvolvimento da Api de Cliente - Parte 2

--- clientes/src/main/java/com/github/micheleascoli/clientes/model/entity/Cliente.java
+++ clientes/src/main/java/com/github/micheleascoli/clientes/model/entity/Cliente.java
@@ -1,9 +1,16 @@
 package com.github.micheleascoli.clientes.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+
 import java.time.LocalDate;
+
 
 @Entity
 @Data  //Lombok dependency e plugin
@@ -17,12 +24,22 @@ public class Cliente {
     private Integer id;
 
     @Column(nullable = false, length = 150)
+    @NotEmpty(message = "{campo.nome.obrigatorio}")
     private String nome;
 
     @Column(nullable = false, length = 11)
+    @NotNull(message = "{campo.cpf.obrigatorio}")
+    @CPF(message = "{campo.cpf.invalido}")
     private String cpf;
 
-    @Column(name= "data_cadastro")
+    @Column(name= "data_cadastro", updatable = false)
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataCadastro;
+
+    @PrePersist
+    public void prePersist(){
+        setDataCadastro(LocalDate.now());
+
+    }
 
 }
