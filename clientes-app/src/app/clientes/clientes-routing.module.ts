b4bbc62001d12728f53c94f8e98bff9a9b942be1
Michele Ascoli <micheleascoli17@gmail.com>
2022-06-23 09:55:15 -0500

    Finalização da aplicação.
    Implementação de Guards, para proteção das URLs. Definindo regras de acesso.

--- clientes-app/src/app/clientes/clientes-routing.module.ts
+++ clientes-app/src/app/clientes/clientes-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../auth.guard';
 import { LayoutComponent } from '../layout/layout.component';
 import { LoginComponent } from '../login/login.component';
 
@@ -7,7 +8,7 @@ import { ClientesFormComponent } from './clientes-form/clientes-form.component';
 import { ClientesListaComponent } from './clientes-lista/clientes-lista.component';
 
 const routes: Routes = [
-  { path: 'clientes', component: LayoutComponent , children: [
+  { path: 'clientes', component: LayoutComponent , canActivate: [AuthGuard], children: [
      { path: 'form', component: ClientesFormComponent },
      { path: 'form/:id', component: ClientesFormComponent },
      { path: 'lista', component: ClientesListaComponent},
